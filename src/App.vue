11b3fdfc209722a86cdd2374dbc0284320881d36
Felix falk <felix.falk2903@gmail.com>
2023-12-26 13:38:32 +0100

    basis

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <FienPortfolio path=""/>
+</template>
+
+<script>
+import FienPortfolio from './components/portfolio.vue'
+
+export default {
+  name: 'App',
+  components: {
+    FienPortfolio
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/portfolio.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <pdf src="../assets/StudentAtWork-Attest-Felix-Falk.pdf"></pdf>
+  </div>
+</template>
+
+<script>
+import pdf from 'vue-pdf'
+export default {
+  name: 'FienPortfolio',
+  components: {
+    pdf
+  },
+  props: {
+    path: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
